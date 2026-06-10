2360cc0e1b0eaf0946759a5244d1f29a4e3621ce
KentoNishimata <nishimata_kento0918@yahoo.co.jp>
2026-06-10 22:26:20 +0900

    refactor file organizer

--- file_organizer.py
+++ file_organizer.py
@@ -1,13 +1,31 @@
 from pathlib import Path
 
 folder = Path.home()/"Downloads"
+def should_skip(file):
+    if file.name.startswith("."):
+        return True
+
+    extension = file.suffix.replace(".", "")
+
+    if extension == "":
+        return True
+
+    return False
+def move_file(file, folder):
+    extension = file.suffix.replace(".", "")
+
+    target_folder = folder / extension
+    target_folder.mkdir(exist_ok=True)
+
+    destination = target_folder / file.name
+    file.rename(destination)
+
+    print(f"{file.name}を移動")
 
 for file in folder.iterdir():
+    
+    if should_skip(file):
+        continue
+    
     if file.is_file():
-        print(file.name,file.suffix)
-        extension = file.suffix.replace(".","")
-        target_folder=folder/extension
-        target_folder.mkdir(exist_ok=True)
-        destination=target_folder/file.name
-        file.rename(destination)
-        print(f"{file.name}を移動")
+         move_file(file, folder)
